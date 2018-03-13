3fdc4f6f4e7ee957ab83cdaaa7d51774eb9712c4
L <printsnow@qq.com>
2018-03-13 19:57:15 +0800

    入口

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env/ python3 
 # -*- coding: utf-8 -*-
 
+import re
 from search import search, get_pages, choose_novel
 from links import get_links, get_stamp_urls, get_datas
 from multiprocessing import Pool
@@ -12,9 +13,12 @@ def main(url):
     pages = search(url)
     pageall = get_pages(pages)
     book_url = choose_novel(pageall)
-    urls = get_links(book_url)
-    stamp_urls = get_stamp_urls(urls)
-    return stamp_urls
+    if re.match(r'http://www.biquge.com.tw/\d*_\d*/', book_url):
+        urls = get_links(book_url)
+        stamp_urls = get_stamp_urls(urls)
+        return stamp_urls
+    else:
+        return
 
 
 
@@ -22,9 +26,12 @@ if __name__ == '__main__':
     URL = 'http://www.biquge.com.tw/modules/article/soshu.php'
     p = Pool()
     stamp_urls = main(URL)
-    contents = p.map(get_datas, stamp_urls)
-    for content in contents:
-        datas_write(content)
+    if stamp_urls is None:
+        print('sorry!')
+    else:
+        contents = p.map(get_datas, stamp_urls)
+        for content in contents:
+            datas_write(content)
 #    p.map(get_datas, stamp_urls)
 
 
