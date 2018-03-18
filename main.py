babebe5c8de4099cd44b43a9ec704d9930e63e7f
L <printsnow@qq.com>
2018-03-18 20:37:36 +0800

    main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import re
 from search import search, get_pages, choose_novel
 from links import get_links, get_stamp_urls, get_datas
 from multiprocessing import Pool
-from dataswrite import datas_write 
+from dateswrite import datas_write 
 
 global contents
 def main(url):
