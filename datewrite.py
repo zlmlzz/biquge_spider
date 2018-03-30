1172fd4bc550dff307fa7faf4f0dfab8bded1263
L <printsnow@qq.com>
2018-03-30 18:45:24 +0800

    datewrite

--- datewrite.py
+++ datewrite.py
@@ -6,7 +6,6 @@ from search import BOOK_NAME
 def datas_write(datas, bookname=BOOK_NAME):
     if datas is None:
         return
-    print('即将完成!')
     with open('{}.txt'.format(bookname), 'a') as f:
         for num, content in datas.items():
             f.write(content)
